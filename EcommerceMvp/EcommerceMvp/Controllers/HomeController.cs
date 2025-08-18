4afca2376798cf7824dd1da21e76efbc850295e0
AnaMarija777 <anamarija.stojchevska1@gmail.com>
2025-08-18 13:46:18 +0200

    Add TimeTracker project

---
+++ EcommerceMvp/EcommerceMvp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceMvp.Models;
+
+namespace EcommerceMvp.Controllers;
+
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //od tuka
+    [HttpPost]   // ← ова е важно
+    public IActionResult Subscribe(string email)
+    {
+        if (!string.IsNullOrEmpty(email))
+        {
+            TempData["Message"] = $"Thank you! The discount code has been sent to {email}";
+        }
+        else
+        {
+            TempData["Message"] = "Please enter a valid email.";
+        }
+
+        return RedirectToAction("Index");
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+
---
+++ EcommerceMvp/EcommerceMvp/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EcommerceMvp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMvp.Controllers
+{
+
+	
+        public class ProductsController : Controller
+        {
+            
+            
+                public IActionResult Index()
+                {
+                    var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Hydrating Face Cream", Price = 25.99M, ImageUrl = "/images/facecream.jpg", Description = "Moisturizing cream for daily use" },
+                new Product { Id = 2, Name = "Vitamin C Serum", Price = 19.99M, ImageUrl = "/images/serum.jpg", Description = "Brightens and revitalizes skin" },
+                new Product { Id = 3, Name = "Gentle Cleanser", Price = 15.50M, ImageUrl = "/images/cleansinggel.jpg", Description = "Removes impurities without drying" }
+            };
+
+                    return View(products);
+                }
+            }
+        }
+
+    
+
+
+    
+
+
